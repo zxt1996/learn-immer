4c0084c3e1b239f4809e39b6dcec3f4bc0ad5b6e
zxt1996 <15625144271@163.com>
2020-12-18 16:10:32 +0800

    produce柯里化运用

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React, { useEffect } from 'react';
+// import Users from './components/users';
 // import { test } from './demo/about-produce';
-import Users from './components/users';
+import { test } from './demo/about-producer';
 
 function App() {
-  // useEffect(() => {
-  //   test();
-  // }, []);
+  useEffect(() => {
+    test();
+  }, []);
   return (
     <div className="App">
-      <Users/>
+      {/* <Users/> */}
     </div>
   );
 }
--- src/demo/about-producer.ts
+++ src/demo/about-producer.ts
@@ -1,2 +1,10 @@
 import produce from 'immer';
 
+// 柯里化，produce中接收的参数就是mapper接收的参数
+const mapper = produce((draft, index) => {
+    draft.index = index;
+})
+
+export const test = () => {
+    console.log([{}, {}, {}].map(mapper)) //[{index: 0}, {index: 1}, {index: 2}])
+}
